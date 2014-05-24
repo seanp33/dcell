0a367c2b0ee4fb57b46968abca9ad9a7b81a044c
seanp33 <smonaghan000@gmail.com>
2014-05-24 11:35:51 -0400

    correcting enum order

--- common/src/main/thrift/core.thrift
+++ common/src/main/thrift/core.thrift
@@ -1,11 +1,6 @@
 namespace java dcell.core.thrift
 namespace cpp dcell.core.thrift
 
-exception RemoteException{
-    1: EXTYPE type,
-    2: optional string msg
-}
-
 enum EXTYPE {
     GENERAL_FAULT = 1,
     TIMEOUT = 2,
@@ -13,4 +8,9 @@ enum EXTYPE {
     STALENESS = 4,
     BUSY = 5,
     UNKNOWN = 6
+}
+
+exception RemoteException{
+    1: EXTYPE type,
+    2: optional string msg
 }
